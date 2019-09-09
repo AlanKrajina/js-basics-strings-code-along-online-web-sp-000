47f8d6dcabf3f313125d0f5820a4c6387cc5f91f
Alan Krajina <alankrajina88@gmail.com>
2019-09-09 11:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
-const shortGreeting = '';
+const shortGreeting = 'Welcome, ';
